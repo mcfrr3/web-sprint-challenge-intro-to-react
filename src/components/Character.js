b354cfd3b56cb48a7ed9d8fe30d32e4306d87e15
Mike Ferrete <michael.ferrete@gmail.com>
2022-02-18 09:54:37 -0900

    project passes tests

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from "react";
+
+const Character = props => {
+    const { character } = props;
+
+    return (
+        <div className="sw-characters">
+            <h2>{ character.name }</h2>
+        </div>
+    )
+}
+
+export default Character;
